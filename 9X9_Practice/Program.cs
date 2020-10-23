a8a19a0fec4a9dccdcb5f47ade2c7048d68f6365
sonia1013 <71934074+sonia1013@users.noreply.github.com>
2020-10-23 16:06:39 +0800

    Add files via upload

---
+++ 9X9_Practice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _9X9_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("start");
+            for (var i = 0; i <= 6; i += 3)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (var k = 1; k < 4; k++)
+                        Console.Write((k + i) + "*" + j + "=" + (k + i) * j + '\t');
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
